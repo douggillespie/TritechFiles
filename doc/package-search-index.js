8cc845e1b053aedb594c1b7a42c99e231fcad529
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2024-09-02 17:02:42 +0100

    Add Javadoc to repo

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"tritechgemini.detect"},{"l":"tritechgemini.fileio"},{"l":"tritechgemini.imagedata"}];updateSearchResults();
